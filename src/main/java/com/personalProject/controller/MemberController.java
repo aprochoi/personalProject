121c24c2af532974a9ca7702f075dd5302cee73f
aprochoi <101087622+aprochoi@users.noreply.github.com>
2022-10-24 17:53:21 +0900

    * MemberController modify

--- src/main/java/com/personalProject/controller/MemberController.java
+++ src/main/java/com/personalProject/controller/MemberController.java
@@ -1,9 +1,38 @@
 package com.personalProject.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/members")
 public class MemberController {
+
+    @PostMapping
+    public ResponseEntity postMember(@RequestParam("email") String email,
+                                     @RequestParam("name") String name,
+                                     @RequestParam("phone") String phone) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("email", email);
+        map.put("name", name);
+        map.put("phone", phone);
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{member-id}")
+    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getMembers() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
