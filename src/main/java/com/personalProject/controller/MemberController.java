e76dbc4374f99d624ba9c9e77db7cebb81b6182f
aprochoi <101087622+aprochoi@users.noreply.github.com>
2022-10-25 15:59:12 +0900

    Delete Member, Patch Member 추가 (#6)
    * PatchMember, DeleteMember 추가

--- src/main/java/com/personalProject/controller/MemberController.java
+++ src/main/java/com/personalProject/controller/MemberController.java
@@ -4,12 +4,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/members")
 public class MemberController {
+
+    private final Map<Long, Map<String, Object>> members = new HashMap<>();
+
+    @PostConstruct
+
+    public void init() {
+        Map<String, Object> member1 = new HashMap<>();
+        long memberId = 1L;
+        member1.put("memberId", memberId);
+        member1.put("email", "hgd@gamil.com");
+        member1.put("name", "홍길동");
+        member1.put("phone", "010-1234-5678");
+
+        members.put(memberId, member1);
+    }
 
     @PostMapping
     public ResponseEntity postMember(@RequestParam("email") String email,
@@ -34,5 +50,25 @@ public class MemberController {
     public ResponseEntity getMembers() {
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-id") long memberId,
+                                      @RequestParam("phone") String phone) {
+
+        Map<String, Object> updateMember = members.get(memberId);
+
+        updateMember.put("phone", phone);
+
+        return new ResponseEntity<>(updateMember, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
+
+        members.remove(memberId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
