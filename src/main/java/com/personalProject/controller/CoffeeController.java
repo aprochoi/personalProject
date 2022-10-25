43efc6f187617402f682a28f34a8038e0623857a
aprochoi <101087622+aprochoi@users.noreply.github.com>
2022-10-25 14:53:14 +0900

    Coffee Controller add (#5)

--- src/main/java/com/personalProject/controller/CoffeeController.java
+++ src/main/java/com/personalProject/controller/CoffeeController.java
@@ -1,9 +1,39 @@
 package com.personalProject.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/coffees")
 public class CoffeeController {
+
+    @PostMapping
+    public ResponseEntity postCoffee(@RequestParam("engName") String engName,
+                                     @RequestParam("korName") String korName,
+                                     @RequestParam("price") long price) {
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("engName", engName);
+        map.put("korName", korName);
+        map.put("price", price);
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{coffee-id}")
+    public ResponseEntity getCoffee(@PathVariable("coffee-id") long coffeeId) {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getCoffees() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
